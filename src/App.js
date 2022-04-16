121a13f8d6fba842fa0124f8f886b9a7ce548fe7
waldoeGeek <thomas.reynolds@student.sl.on.ca>
2022-04-16 00:47:11 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Categories-container'>
+      <div classname='category-container'>
+        {/*<img src='#' alt='category' classname='category-img' />*/}
+        <div class='category-body-container'>
+          <h2>hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div classname='category-container'>
+        {/*<img src='#' alt='category' classname='category-img' />*/}
+        <div class='category-body-container'>
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div classname='category-container'>
+        {/*<img src='#' alt='category' classname='category-img' />*/}
+        <div class='category-body-container'>
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div classname='category-container'>
+        {/*<img src='#' alt='category' classname='category-img' />*/}
+        <div class='category-body-container'>
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div classname='category-container'>
+        {/*<img src='#' alt='category' classname='category-img' />*/}
+        <div class='category-body-container'>
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
