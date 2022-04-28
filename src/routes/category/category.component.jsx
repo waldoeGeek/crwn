07b0d02dd0afcfdac646507d0ed29fea8111ddb9
Thomas Reynolds <web@treynolds.ca>
2022-04-28 01:59:24 -0400

    added links in category

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,5 @@
-import { useParams } from 'react-router-dom';
-import { useContext, useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { useContext, useState, useEffect, Fragment } from 'react';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
@@ -12,13 +12,50 @@ const Category = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
+  const GetCategoryLinks = () => {
+    if (category === 'hats') {
+      return ['jackets', 'sneakers', 'womens', 'mens'];
+    } else if (category === 'jackets') {
+      return ['hats', 'sneakers', 'womens', 'mens'];
+    } else if (category === 'sneakers') {
+      return ['hats', 'jackets', 'womens', 'mens'];
+    } else if (category === 'womens') {
+      return ['hats', 'jackets', 'sneakers', 'mens'];
+    } else if (category === 'mens') {
+      return ['hats', 'jackets', 'sneakers', 'womens'];
+    }
+  };
+
+  const getCategoryLinks = GetCategoryLinks();
+  console.log(`array: ${getCategoryLinks}`);
+
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
   return (
     <div className='category-container'>
-      <h2 className='category__title'>{category}</h2>
+      <div className='category__title'>
+        <h2>{category}</h2>
+        <hr />
+        <div className='category__links'>
+          {getCategoryLinks.map((item, idx) => {
+            return (
+              <Link to={`/shop/${item}`}>
+                <Fragment key={Math.random()}>
+                  <h3
+                    style={{
+                      animation: `fade 1.${idx}s ease-in-out`,
+                    }}
+                  >
+                    {item}
+                  </h3>
+                </Fragment>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
       {products &&
         products.map((product) => (
           <ProductCard key={product.id} product={product} />
